08ef4cf13a20207a771d92ede7ff308464e1e720
mikep <mike.polynin@gmail.com>
2019-01-14 22:15:16 +0300

    1

--- com/itmo/air/mysql/rest/MessageController.java
+++ com/itmo/air/mysql/rest/MessageController.java
@@ -1,12 +1,17 @@
 package com.itmo.air.mysql.rest;
 
+import com.itmo.air.mysql.entity.Account;
 import com.itmo.air.mysql.entity.Message;
+import com.itmo.air.mysql.repo.AccountRepository;
 import com.itmo.air.mysql.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 @RestController
@@ -14,18 +19,36 @@ public class MessageController {
 
     @Autowired
     MessageService messageService;
+    @Autowired
+    AccountRepository accountRepository;
 
     @GetMapping("/users/{usersId}/accounts/{accountId}/messages")
-    public Set<Message> getMessagesByAccountId(@PathVariable(value = "accountId") Long accountId,
+    public Set<Message> getMessagesByAccountId(@PathVariable(value = "userId") Long userId,
+                                               @RequestHeader(value = "token") String userToken,
+                                               @Valid @RequestBody Account account,
                                                Pageable pageable) {
-        return messageService.findMessagesByAccountId(accountId, pageable);
+        MessageService.Result result = messageService.findMessagesByAccountId(userId, userToken, account, pageable);
+        switch (result) {
+            case Success:
+                return accountRepository.findByNicName(account.getNicName()).get().getMessages(pageable);
+            case No_such_account:
+                return "No such account";
+            case No_such_user:
+                return "No such user";
+            case Unauthorized_403:
+                return "Error 403. Unauthorized";
+        }
     }
 
     @PostMapping("/users/{usersId}/accounts/{accountId}/messages/create")
-    public String createMessage(@PathVariable(value = "accountId") Long accountId,
+    public String createMessage(@PathVariable(value = "userId") Long userId,
+                                @PathVariable(value = "accountId") Long accountId,
+                                @RequestHeader(value = "userToken") String userToken,
+                                @RequestHeader(value = "accountToken") String accountToken,
+                                @RequestHeader(value = "nicName") String nicName,
                                 @Valid @RequestBody Message message,
                                 Pageable pageable) {
-        MessageService.Result result = messageService.createMessage(accountId, message, pageable);
+        MessageService.Result result = messageService.createMessage(userId, accountId, userToken, accountToken, nicName, message, pageable);
         switch (result) {
             case Wrong_sender:
                 return "Wrong sender nicName";
@@ -33,6 +56,12 @@ public class MessageController {
                 return "No recipients with such nicName";
             case Message_has_been_sent:
                 return "Message has been sent";
+            case No_such_user:
+                return "No such user";
+            case No_such_account:
+                return "No such account";
+            case Unauthorized_403:
+                return "Error 403. Unauthorized";
             default:
                 return "Unknown error 503";
         }
--- com/itmo/air/mysql/service/MessageService.java
+++ com/itmo/air/mysql/service/MessageService.java
@@ -1,12 +1,17 @@
 package com.itmo.air.mysql.service;
 
+import com.itmo.air.mysql.entity.Account;
 import com.itmo.air.mysql.entity.Message;
+import com.itmo.air.mysql.entity.User;
 import com.itmo.air.mysql.repo.AccountRepository;
 import com.itmo.air.mysql.repo.MessageRepository;
+import com.itmo.air.mysql.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -15,33 +20,81 @@ public class MessageService {
     public enum Result {
         Wrong_sender,
         No_such_recipient,
-        Message_has_been_sent
+        Message_has_been_sent,
+        No_such_account,
+        Success,
+        Unauthorized_403,
+        No_such_user
     }
 
     @Autowired
     MessageRepository messageRepository;
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    UserRepository userRepository;
 
-
-    public Set<Message> findMessagesByAccountId(Long accountId, Pageable pageable) {
-        return accountRepository.findById(accountId).get().getMessages(pageable);
+    public Result findMessagesByAccountId(Long userId, String userToken, Account account, Pageable pageable) {
+        if (userRepository.findById(userId).isPresent()) {
+            User newUser = userRepository.findById(userId).get();
+            if (newUser.getToken().equals(userToken)) {
+                List<String> ids = new ArrayList<>();
+                for (Account acc : newUser.getAccounts()) {
+                    ids.add(acc.getNicName());
+                }
+                if (!ids.contains(account.getNicName())) {
+                    return Result.No_such_account;
+                } else {
+                    Account newAccount = accountRepository.findById(account.getId()).get();
+                    if (newAccount.getToken().equals(account.getToken())) {
+                        return Result.Success;
+                    } else {
+                        return Result.Unauthorized_403;
+                    }
+                }
+            } else {
+                return Result.Unauthorized_403;
+            }
+        } else {
+            return Result.No_such_user;
+        }
     }
 
-    public Result createMessage(Long accountId, Message message, Pageable pageable) {
-        if (!accountRepository.findById(accountId).isPresent() |
-                !accountRepository.findById(accountId).get().getNicName().equals(message.getFrom())) {
-            return Result.Wrong_sender;
-        } else {
-            for (String rec : message.getTo()) {
-                if (!accountRepository.findByNicName(rec).isPresent()) {
-                    return Result.No_such_recipient;
-                } else {
-                    accountRepository.findByNicName(rec).get().getMessages(pageable).add(message);
+    public Result createMessage(Long userId, Long accountId, String userToken, String accountToken, String nicName, Message message, Pageable pageable) {
+        if (userRepository.findById(userId).isPresent()) {
+            User newUser = userRepository.findById(userId).get();
+            if (newUser.getToken().equals(userToken)) {
+                List<String> ids = new ArrayList<>();
+                for (Account acc : newUser.getAccounts()) {
+                    ids.add(acc.getNicName());
                 }
+                if (!ids.contains(nicName)) {
+                    return Result.No_such_account;
+                } else {
+                    Account newAccount = accountRepository.findById(accountId).get();
+                    if (newAccount.getToken().equals(accountToken)) {
+                        if (!newAccount.getNicName().equals(message.getFrom())) {
+                            return Result.Wrong_sender;
+                        } else {
+                            for (String rec : message.getTo()) {
+                                if (!accountRepository.findByNicName(rec).isPresent()) {
+                                    return Result.No_such_recipient;
+                                } else {
+                                    accountRepository.findByNicName(rec).get().getMessages(pageable).add(message);
+                                }
+                            }
+                            messageRepository.save(message);
+                            return Result.Message_has_been_sent;
+                        }
+                    } else {
+                        return Result.Unauthorized_403;
+                    }
+                }
+            } else {
+                return Result.Unauthorized_403;
             }
-            messageRepository.save(message);
-            return Result.Message_has_been_sent;
+        } else {
+            return Result.No_such_user;
         }
     }
 }
